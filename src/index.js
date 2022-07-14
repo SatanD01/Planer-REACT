a79e45fb0856ddc2d781939c896acf5eb17f9905
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-07-14 20:36:38 +0500

    null

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App />);
+root.render(
+    <App />
+);
